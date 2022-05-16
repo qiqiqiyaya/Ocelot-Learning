b25a7349c2af11811a3872019db6e9f0d8e664ec
youli <www.xingjijiandui@aliyun.com>
2022-05-16 22:39:45 +0800

    test one case

--- Ocelot-App/Ocelot-App/Program.cs
+++ Ocelot-App/Ocelot-App/Program.cs
@@ -1,37 +1,36 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    ApplicationName = typeof(Program).Assembly.FullName,
+    ContentRootPath = Path.GetFullPath(Directory.GetCurrentDirectory()),
+    WebRootPath = "wwwroot",
+    Args = args
+});
 builder.WebHost
-    .UseContentRoot(Directory.GetCurrentDirectory())
     .ConfigureAppConfiguration((hostingContext, config) =>
     {
         config
-        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-        .AddJsonFile("appsettings.json",true,true)
-        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-        .AddJsonFile("ocelot.json")
-        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
-    })
-    .ConfigureServices(s =>
-    {
-        s.AddOcelot();
+            .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+            .AddJsonFile("ocelot.json")
+            .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
     })
     .ConfigureLogging((hostingContext, logging) =>
     {
         logging.AddConsole();
     })
-    .UseIISIntegration()
-    .Configure(app =>
-    {
-        app.UseOcelot().Wait();
-    });
+    .UseIISIntegration();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
+app.UseOcelot().Wait();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -41,7 +40,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapRazorPages();
 
